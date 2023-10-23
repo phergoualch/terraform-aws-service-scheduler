22f0ce5bdc2765db249f09f4fc348b03eb539ce8
Pereg Hergoualc'h <pereg.hergoualch@revolve.team>
2023-10-23 13:47:57 +0200

    feat: adding new tags support, active-days-month and active-months

--- lambda/src/tools.py
+++ lambda/src/tools.py
@@ -65,6 +65,8 @@ def get_resource_values(tags, action):
     resource_values = {
         "event_time": None,
         "active_days": "MON-SUN",
+        "active_days_month": "1-31",
+        "active_months": "JAN-DEC",
         "active_weeks": "1-53",
         "event_timezone": os.environ.get("DEFAULT_TIMEZONE"),
     }
@@ -72,16 +74,34 @@ def get_resource_values(tags, action):
     for tag in tags:
         if tag["key"] == f"finops:{action}-time":
             resource_values["event_time"] = tag["value"]
+        elif tag["key"] == f"finops:timezone":
+            resource_values["event_timezone"] = tag["value"]
+
         elif tag["key"] == "finops:active-days":
             resource_values["active_days"] = tag["value"]
         elif tag["key"] == f"finops:{action}-active-days":
             resource_values["active_days"] = tag["value"]
-        elif tag["key"] == f"finops:timezone":
-            resource_values["event_timezone"] = tag["value"]
+
+        elif tag["key"] == "finops:active-days-month":
+            resource_values["active_days_month"] = tag["value"]
+        elif tag["key"] == f"finops:{action}-active-days-month":
+            resource_values["active_days_month"] = tag["value"]
+
+        elif tag["key"] == "finops:active-months":
+            resource_values["active_months"] = tag["value"]
+        elif tag["key"] == f"finops:{action}-active-months":
+            resource_values["active_months"] = tag["value"]
+
+        elif tag["key"] == "finops:active-days":
+            resource_values["active_days"] = tag["value"]
+        elif tag["key"] == f"finops:{action}-active-days":
+            resource_values["active_days"] = tag["value"]
+
         elif tag["key"] == f"finops:active-weeks":
             resource_values["active_weeks"] = tag["value"]
         elif tag["key"] == f"finops:{action}-active-weeks":
             resource_values["active_weeks"] = tag["value"]
+
         elif tag["key"] == f"finops:enabled" and tag["value"] != "true":
             return None
 
@@ -167,6 +187,111 @@ def is_day_in_range(day, day_range):
     return False
 
 
+def is_day_in_range_month(day, day_range):
+    """
+    Check if a given day is in a given day range
+
+    Parameters
+    ----------
+    day : str
+        The day to check
+    day_range : str
+        The day range to check against
+        Format: 1-31 or 1,2,3 or 1,2-4,31 or 1
+
+    Returns
+    -------
+    bool
+    """
+    day_range = day_range.upper()
+
+    # Split the day_range string into individual day parts
+    day_parts = day_range.split(",")
+
+    for part in day_parts:
+        if "-" in part:
+            start_day, end_day = part.split("-")
+            if int(start_day) <= int(end_day):
+                if int(start_day) <= int(day) <= int(end_day):
+                    return True
+            else:
+                if int(start_day) <= int(day) or int(day) <= int(end_day):
+                    return True
+        else:
+            if "," in part:
+                sub_parts = part.split("-")
+                for sub_part in sub_parts:
+                    if int(day) == int(sub_part):
+                        return True
+            else:
+                if int(day) == int(part):
+                    return True
+
+    return False
+
+
+def is_month_in_range(month, month_range):
+    """
+    Check if a given month is in a given month range
+
+    Parameters
+    ----------
+    month : str
+        The month to check
+    month_range : str
+        The month range to check against
+        Format: JAN-DEC or JAN,FEB,MAR or JAN,FEB-APR,DEC or JAN
+
+    Returns
+    -------
+    bool
+    """
+    month_range = month_range.upper()
+
+    # Split the month_range string into individual month parts
+    month_parts = month_range.split(",")
+
+    # Define a dictionary to map month abbreviations to their respective numeric values
+    month_mapping = {
+        "JAN": 1,
+        "FEB": 2,
+        "MAR": 3,
+        "APR": 4,
+        "MAY": 5,
+        "JUN": 6,
+        "JUL": 7,
+        "AUG": 8,
+        "SEP": 9,
+        "OCT": 10,
+        "NOV": 11,
+        "DEC": 12,
+    }
+
+    for part in month_parts:
+        if "-" in part:
+            start_month, end_month = part.split("-")
+            start_val = month_mapping[start_month]
+            end_val = month_mapping[end_month]
+
+            if start_val <= end_val:
+                if start_val <= month_mapping[month] <= end_val:
+                    return True
+            else:
+                if start_val <= month_mapping[month] or month_mapping[month] <= end_val:
+                    return True
+        else:
+            if "," in part:
+                sub_parts = part.split("-")
+                for sub_part in sub_parts:
+                    if month_mapping[month] == month_mapping[sub_part]:
+                        return True
+            else:
+                if month_mapping[month] == month_mapping[part]:
+                    return True
+
+    return False
+
+
 def is_week_in_range(week, week_range):
     """
     Check if a given week is in a given week range
@@ -240,6 +365,8 @@ def get_next_event_datetime(resource_values):
     if (
         is_day_in_range(current_datetime.strftime("%a").upper(), resource_values["active_days"])
         and is_week_in_range(current_datetime.strftime("%U"), resource_values["active_weeks"])
+        and is_month_in_range(current_datetime.strftime("%b").upper(), resource_values["active_months"])
+        and is_day_in_range_month(current_datetime.strftime("%d"), resource_values["active_days_month"])
         and current_datetime.time() < event_time
     ):
         next_event_datetime = current_datetime.replace(
@@ -248,9 +375,12 @@ def get_next_event_datetime(resource_values):
     else:
         # Find the next occurrence of the event
         next_day = current_datetime + timedelta(days=1)
-        while not is_day_in_range(
-            next_day.strftime("%a").upper(), resource_values["active_days"]
-        ) and not is_week_in_range(next_day.strftime("%U"), resource_values["active_weeks"]):
+        while (
+            not is_day_in_range(next_day.strftime("%a").upper(), resource_values["active_days"])
+            and not is_week_in_range(next_day.strftime("%U"), resource_values["active_weeks"])
+            and not is_month_in_range(next_day.strftime("%b").upper(), resource_values["active_months"])
+            and not is_day_in_range_month(next_day.strftime("%d"), resource_values["active_days_month"])
+        ):
             next_day += timedelta(days=1)
 
         return next_day.replace(hour=event_time.hour, minute=event_time.minute, second=0, microsecond=0)
