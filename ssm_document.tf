a8ab42ec37bde1a73e74c7372517b363ccae2689
Pereg Hergoualc'h <pereg.hergoualch@revolve.team>
2023-12-01 22:43:56 +0100

    fix(document): converted ssm document from yaml to json and added variable for state machine name

--- ssm_document.tf
+++ ssm_document.tf
@@ -5,31 +5,41 @@ data "aws_caller_identity" "current" {}
 resource "aws_ssm_document" "manual" {
   name            = "${var.app_name}-manual"
   document_type   = "Automation"
-  document_format = "YAML"
+  document_format = "JSON"
 
-  content = <<DOC
-description: ''
-schemaVersion: '0.3'
-parameters:
-  action:
-     type: String
-     allowedValues:
-       - start
-       - stop
-     description: "The action to execute, either start or stop"
-  selector:
-    type: String
-    default: "all"
-    description: "The tags to select the resources, in a key=value format, default to all"
-  services:
-    type: String
-    default: "all"
-    description: "The services to execute the action on, default to all"
-mainSteps:
- - name: InvokeStateMachine
-   action: aws:executeStateMachine
-   inputs:
-     stateMachineArn: arn:aws:states:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:stateMachine:service-scheduler-{{ action }}
-     input: '{"selector": "{{ selector }}", "action": "{{ action }}", "services": "{{ services }}"}'
-DOC
+  content = jsonencode({
+    "description" : "Manually invoke the service scheduler",
+    "schemaVersion" : "0.3",
+    "parameters" : {
+      "selector" : {
+        "type" : "String",
+        "default" : "all",
+        "description" : "The tags to select the resources, in a key=value format, default to all"
+      },
+      "services" : {
+        "type" : "String",
+        "default" : "all",
+        "description" : "The services to execute the action on, default to all"
+      },
+      "action" : {
+        "type" : "String",
+        "allowedValues" : [
+          "start",
+          "stop"
+        ],
+        "description" : "The action to execute, either start or stop"
+      }
+    },
+    "mainSteps" : [
+      {
+        "name" : "InvokeStateMachine",
+        "action" : "aws:executeStateMachine",
+        "isEnd" : true,
+        "inputs" : {
+          "input" : "{\"selector\": \"{{ selector }}\", \"action\": \"{{ action }}\", \"services\": \"{{ services }}\"}",
+          "stateMachineArn" : "arn:aws:states:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:stateMachine:${var.app_name}-{{ action }}"
+        }
+      }
+    ]
+  })
 }
