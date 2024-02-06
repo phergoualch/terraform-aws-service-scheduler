9cf601b55fe9f0d54bd30854d93db717cb282a56
Pereg Hergoualc'h <pereg.hergoualch@revolve.team>
2024-02-06 16:09:31 +0100

    test: added Python tests using pytest

---
+++ lambda/src/tests/test_global.py
@@ -0,0 +1,149 @@
+import pytest
+from services import EC2
+from models import Resource, Tag
+from models.enums import Action
+from utils.tools import check_selector_tags
+from datetime import timedelta
+from dateutil import tz
+
+
+@pytest.fixture
+def service():
+    action = Action.START
+    parameters = {
+        "tags_prefix": "scheduler",
+        "tags_mapping": {
+            tag: tag
+            for tag in [
+                "enabled",
+                "time",
+                "timezone",
+                "active-days",
+                "active-days-of-month",
+                "active-weeks",
+                "active-months",
+                "parameter",
+            ]
+        },
+        "default_timezone": "UTC",
+        "interval": "6",
+    }
+    service = EC2(action, parameters)
+    return service
+
+
+@pytest.fixture
+def resource(service):
+    resource = Resource(
+        "arn",
+        service,
+        [
+            Tag("scheduler:enabled", "true"),
+            Tag("scheduler:start-time", "10:00"),
+            Tag("scheduler:timezone", "UTC"),
+        ],
+        {"id": "id"},
+    )
+    return resource
+
+
+def test_tag():
+    tag = Tag("key", "value")
+    assert tag.key == "key"
+    assert tag.value == "value"
+
+
+def test_service(service):
+    assert service.name == "ec2"
+    assert service.action == Action.START
+    assert service.tags_prefix == "scheduler"
+    assert service.default_timezone == "UTC"
+    assert service.interval == 6
+    assert service.tags_mapping == {
+        tag: tag
+        for tag in [
+            "enabled",
+            "time",
+            "timezone",
+            "active-days",
+            "active-days-of-month",
+            "active-weeks",
+            "active-months",
+            "parameter",
+        ]
+    }
+
+
+def test_resource(resource):
+    assert resource.id == "arn"
+    assert all([isinstance(tag, Tag) for tag in resource.tags])
+    assert resource.attributes == {"id": "id"}
+
+
+def test_resource_is_enabled(resource):
+    assert resource.enabled == True
+
+
+def test_resource_is_enabled_missing_tag(service):
+    resource = Resource("arn", service, [Tag("scheduler:wrong_tag", "true")])
+    assert resource.enabled == False
+
+
+def test_get_tag_key(service):
+    assert service.get_tag_key("enabled") == "scheduler:enabled"
+    assert service.get_tag_key("time", action=True) == "scheduler:start-time"
+    assert service.get_tag_key("active-months", action=True, iterator=3) == "scheduler:start-active-months:3"
+    assert service.get_tag_key("active-days", iterator=1) == "scheduler:active-days:1"
+
+
+@pytest.fixture
+def resource1(service):
+    resource = Resource(
+        "arn",
+        service,
+        [
+            Tag("scheduler:enabled", "true"),
+            Tag("application", "app1"),
+            Tag("environment", "dev"),
+            Tag("team", "team1"),
+        ],
+    )
+    return resource
+
+
+@pytest.mark.parametrize("delay", [0, 5, 10])
+def test_manual_delay(delay, resource):
+    selectors = [{"services": "ec2", "tags": "all", "delay": delay}]
+    resource.get_next_execution_time_manual(selectors=selectors)
+
+    assert resource.next_execution_time == (resource.service.now + timedelta(minutes=delay)).replace(microsecond=0, tzinfo=tz.tzlocal())
+
+
+@pytest.fixture
+def resource2(service):
+    resource = Resource(
+        "arn",
+        service,
+        [
+            Tag("scheduler:enabled", "true"),
+            Tag("application", "app2"),
+            Tag("environment", "prod"),
+            Tag("team", "team1"),
+        ],
+    )
+    return resource
+
+
+@pytest.mark.parametrize(
+    "selector, expected",
+    [
+        ("all", [True, True]),
+        ("any", [False, False]),
+        ("application=app1", [True, False]),
+        ("application=app1,application=app2", [True, True]),
+        ("team=team1", [True, True]),
+    ],
+)
+def test_check_selector_tags(selector, expected, resource1, resource2):
+    assert check_selector_tags(resource1.tags, selector) == expected[0]
+    assert check_selector_tags(resource2.tags, selector) == expected[1]
---
+++ lambda/src/tests/test_schedule.py
@@ -0,0 +1,125 @@
+from datetime import datetime, timedelta
+
+import pytest
+from dateutil import tz
+from models import Resource, Tag
+from models.enums import Day, Month, IteratorType
+from models.iterator import Iterator
+from utils.tools import is_in_range
+
+from tests.test_global import service
+
+
+@pytest.fixture
+def resource(service):
+    resource = Resource(
+        "arn",
+        service,
+        [
+            Tag("scheduler:enabled", "true"),
+            Tag("scheduler:start-time", "10:00"),
+            Tag("scheduler:timezone", "UTC"),
+            Tag("app", "test"),
+        ],
+        {"id": "id"},
+    )
+    return resource
+
+
+def test_schedule(resource):
+    schedule = resource.get_schedule_from_tags()
+    assert schedule.time == "10:00"
+    assert schedule.timezone == "UTC"
+
+
+def test_is_day_in_range():
+    assert is_in_range(Day.MON.name, "MON-SUN", range_enum=Day) == True
+    assert is_in_range(Day.TUE.name, "SAT-WED", range_enum=Day) == True
+    assert is_in_range(Day.TUE.name, "FRI-SUN", range_enum=Day) == False
+
+
+def test_is_month_in_range():
+    assert is_in_range(Month.MAR.name, "JAN-APR", range_enum=Month) == True
+    assert is_in_range(Month.JUL.name, "JUL,DEC", range_enum=Month) == True
+    assert is_in_range(Month.AUG.name, "JAN,APR-JUL,OCT", range_enum=Month) == False
+
+
+def test_is_number_in_range():
+    assert is_in_range(17, "10-31") == True
+    assert is_in_range(3, "1,3,7") == True
+    assert is_in_range(0, "1-31") == False
+    assert is_in_range(32, "1-31") == False
+    assert is_in_range(17, "1-16,18-31") == False
+    assert is_in_range(23, "17-28,19-37") == True
+
+
+def test_get_next_execution_time(resource):
+    resource.service.now = datetime(2024, 1, 26, 8, 0, 0, tzinfo=tz.gettz("UTC"))
+    schedule = resource.get_schedule_from_tags()
+    assert schedule.get_next_execution_time() == datetime(2024, 1, 26, 10, 0, 0, tzinfo=tz.gettz("UTC"))
+    resource.service.now = datetime(2024, 1, 26, 14, 0, 0, tzinfo=tz.gettz("UTC"))
+    schedule = resource.get_schedule_from_tags()
+    assert schedule.get_next_execution_time() == None
+
+
+def test_get_next_execution_time_next_day(service):
+    resource = Resource(
+        "arn",
+        service,
+        [
+            Tag("scheduler:enabled", "true"),
+            Tag("scheduler:start-time", "03:00"),
+            Tag("scheduler:timezone", "UTC"),
+            Tag("scheduler:active-days", "WED-FRI, SAT"),
+        ],
+    )
+    resource.service.now = datetime(2024, 1, 26, 22, 0, 0, tzinfo=tz.gettz("UTC"))
+    schedule = resource.get_schedule_from_tags()
+    assert schedule.get_next_execution_time() == datetime(2024, 1, 27, 3, 0, 0, tzinfo=tz.gettz("UTC"))
+    resource.service.now = datetime(2024, 1, 26, 18, 0, 0, tzinfo=tz.gettz("UTC"))
+    schedule = resource.get_schedule_from_tags()
+    assert schedule.get_next_execution_time() == None
+
+
+def test_iterators(service):
+    resource = Resource(
+        "arn",
+        service,
+        [
+            Tag("scheduler:enabled", "true"),
+            Tag("scheduler:start-time", "09:00"),
+            Tag("scheduler:active-days", "MON-FRI"),
+            Tag("scheduler:start-time:1", "13:00"),
+            Tag("scheduler:active-days:1", "SAT-SUN"),
+            Tag("scheduler:parameter:2", "test"),
+        ],
+    )
+    assert resource.iterators == [
+        Iterator(iterator=None, type=IteratorType.TAG),
+        Iterator(iterator=1, type=IteratorType.TAG),
+        Iterator(iterator=2, type=IteratorType.PARAMETER),
+    ]
+
+
+def test_schedules_iterators(service):
+    resource = Resource(
+        "arn",
+        service,
+        [
+            Tag("scheduler:enabled", "true"),
+            Tag("scheduler:start-time", "09:00"),
+            Tag("scheduler:active-days", "MON-FRI"),
+            Tag("scheduler:active-months", "JAN-DEC"),
+            Tag("scheduler:start-time:1", "13:00"),
+            Tag("scheduler:active-days:1", "SAT-SUN"),
+        ],
+    )
+    schedules = []
+    for iterator in resource.iterators:
+        schedule = resource.get_schedule_from_tags(iterator=iterator.iterator)
+        schedules.append(schedule)
+
+    assert len(schedules) == 2
+    assert schedules[0].active_days == "MON-FRI"
+    assert schedules[0].active_months == "JAN-DEC"
+    assert schedules[1].active_months == "JAN-DEC"
