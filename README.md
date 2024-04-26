4f968a279bb7ebf12252f1c5c2da102e94067c48
Pereg Hergoualc'h <pereg.hergoualch@revolve.team>
2024-04-26 17:01:30 +0200

    feat(services): adding support for scheduling cloudwatch alarms

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ module "service_scheduler" {
   source = "phergoualch/service-scheduler/aws"
   version = ">= 2.0.0"
 
-  enabled_services   =  ["ec2", "asg", "ecs", "rds", "documentdb", "lambda", "apprunner", "aurora", "elasticache"]
+  enabled_services   =  ["ec2", "asg", "ecs", "rds", "documentdb", "lambda", "apprunner", "aurora", "elasticache", "cloudwatch"]
   default_timezone   = "Europe/Paris"
   app_name           = "service-scheduler"
   execution_interval = 6
---
+++ lambda/src/services/cloudwatch.py
@@ -0,0 +1,52 @@
+import logging
+from typing import Dict, List
+
+from models import Resource, Tag, Service
+from models.enums import Action
+
+
+logger = logging.getLogger(__name__)
+
+
+class Cloudwatch(Service):
+    def __init__(self, action: Action, parameters: Dict = None):
+        super().__init__("cloudwatch", action, parameters)
+
+    def list_resources(self) -> List[Resource]:
+        """
+        Get all Cloudwatch alarms in the account and return them as a list of Resource objects.
+
+        Returns
+        -------
+        resources : List[Resource]
+        """
+        resources = []
+        paginator = self.client.get_paginator("describe_alarms")
+
+        logger.info("Listing Cloudwatch alarms")
+
+        try:
+            for page in paginator.paginate(AlarmTypes=["CompositeAlarm", "MetricAlarm"]):
+                for alarm in page["MetricAlarms"] + page["CompositeAlarms"]:
+                    try:
+                        tags = self.client.list_tags_for_resource(ResourceARN=alarm["AlarmArn"])
+                    except Exception as e:
+                        logger.warning(
+                            f"Error listing tags for Cloudwatch alarm {alarm['AlarmArn']}: {e}"
+                        )
+                        continue
+
+                    resources.append(
+                        Resource(
+                            id_=alarm["AlarmArn"],
+                            service=self,
+                            tags=set([Tag(tag["Key"], tag["Value"]) for tag in tags["Tags"]]),
+                            attributes={"name": alarm["AlarmName"]},
+                        )
+                    )
+            logger.info(f"Found {len(resources)} Cloudwatch alarms")
+            return resources
+
+        except Exception as e:
+            logger.error(f"Error listing Cloudwatch alarms: {e}")
+            raise e
